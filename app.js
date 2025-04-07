6b2d2cc9b591da4f636a2f4566b45fc988479feb
Karina-SaRi19 <2022371122@uteq.edu.mx>
2025-04-07 02:18:59 -0600

    actualizacion nueve

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const { addDays, addHours, addMinutes, addWeeks } = require("date-fns");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
+const cors = require('cors');
 
 const ternuServiceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
@@ -27,7 +28,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://ternurines-front.onrender.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Registro de usuario
 app.post('/register', async (req, res) => {
